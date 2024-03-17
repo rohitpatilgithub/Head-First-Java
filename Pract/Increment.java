6c0a3d36fc6a94ed501bff66d4d4746fbe80f52b
Rohit <flux.amv25@gmail.com>
2024-03-17 20:28:54 +0530

    Increment.java modified

--- Pract/Increment.java
+++ Pract/Increment.java
@@ -13,7 +13,7 @@ public class Increment {
     public static void main(String[] args) {
         increase[] one = new increase[5];
         for (int i = 0;i<one.length;i++){
-            one[i] = new increase(i+1);
+            one[i] = new increase(i+1); //is this possible one[] = new increase(argument)
         }
         int x = 0;
         while(x<one.length){
