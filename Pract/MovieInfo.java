c9dd67e384f3b85a7a12ba44d93c833f5f4f2854
Rohit <flux.amv25@gmail.com>
2024-03-17 20:26:32 +0530

    MovieInfo.java modified

--- Pract/MovieInfo.java
+++ Pract/MovieInfo.java
@@ -7,6 +7,7 @@ class Movie{
     String Movie;
     int Rating;
     String Genre;
+    int num;
 
     void info(){
         System.out.print("Enter movie name : ");
@@ -16,21 +17,29 @@ class Movie{
         System.out.print("Enter movie genre : ");
         Genre = sc.next();
     }
+    Movie(int num){
+        this.num = num;
+    }
     void output(){
+        System.out.println("movie number is :"+num);
         System.out.println("Movie is : "+Movie);
         System.out.println("Rating is : "+Rating);
         System.out.println("Genre is : "+Genre);
     }
 }
 public class MovieInfo {
+
     public static void main(String[] args) {
         int x = 0;
         Movie[] movi = new Movie[3];
+        for(int i = 0;i<= movi.length-1;i++){
+            movi[i] = new Movie(i+1);
+            movi[i].info();
+        }
+        x = 0;
         while(x<3){
-            movi[x] = new Movie();
-            movi[x].info();
             movi[x].output();
             x+=1;
-        }// can't we gather the info in first place and then display all at once
+        }
     }
 }
