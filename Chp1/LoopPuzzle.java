42c20ccab6f6b98746b7f65e8bf2a666346a4f62
Rohit <flux.amv25@gmail.com>
2024-06-12 06:13:53 +0530

    Modified Chp1

--- Chp1/LoopPuzzle.java
+++ Chp1/LoopPuzzle.java
@@ -6,26 +6,25 @@ public class LoopPuzzle {
         //a noise
         //annoys
         //an oyster
-
         int x = 0;
-        while(x<4){
+        while ( x < 4 ) {
             System.out.print("a");
-        if(x<1){
-            System.out.print(" ");
-        }
-        System.out.print("n");
-        if(x<1){
-            System.out.print("oise");
-            x = x - 1;
-        }
-        if(x==1){
-            System.out.print("noys");
-        }
-        if(x>1){
+            if ( x < 1 ) {
+                System.out.print(" ");
+            }
+            System.out.print("n");
+            if ( x < 1 ) {
+                System.out.print("oise");
+                x = x - 1;
+            }
+            if ( x == 1 ) {
+                System.out.print("noys");
+            }
+            if ( x > 1) {
                 System.out.print(" oyster");
-        }
-        System.out.println();
-        x = x + 2;
+            }
+            System.out.println();
+            x = x + 2;
         }
     }
 }
