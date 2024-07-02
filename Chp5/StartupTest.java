0f6c0f0733e5b302e3b8ff8d9594b646d708e511
Rohit <flux.amv25@gmail.com>
2024-07-02 11:08:32 +0530

    Modified StartupTest.java

--- Chp5/StartupTest.java
+++ Chp5/StartupTest.java
@@ -1,6 +1,7 @@
 package Chp5;
 
 import java.util.Random;
+import java.util.Scanner;
 
 class Startup{
     int [] locationCells = new int[7];       // location of Startup cells
@@ -12,14 +13,32 @@ class Startup{
             locationCells[a] = a+1;
             a++;
         }
-        for (int i = 0; i <= 6 ; i++) {
-            System.out.println(locationCells[i]);
+        System.out.println("Array elements are : ");
+        for (int i = 0; i < 7 ; i++) {
+            System.out.print(" ["+locationCells[i]+"] ");
         }
+    }
+    void startGame(){
+        Scanner sc = new Scanner(System.in);
+        while(numOfHits != 3) {                 // till we get 3 hits
+            System.out.print("\nGuess : ");
+            int a = sc.nextInt();               //  takes input
+            if( a <= 7 ) {                       // your number must be less than 7
+                for(int i = 0 ; i < 7 ; i++) {  //  'a' is checked in every array element possible
+                    if(locationCells[i] == a){  //  if it finds any element equals to 'a' then it returns Hit
+                        System.out.println("Hit");
+                        numOfHits++;
+                    }
+                }
+            }
+        }
+        System.out.println("Kill");
     }
 }
 public class StartupTest {
     public static void main(String[] args) {
         Startup my = new Startup();
         my.setupLocations();
+        my.startGame();
     }
 }
